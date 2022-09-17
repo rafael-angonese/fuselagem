f2a300cb75c93b9310c0b6289bb022cb02f9712b
Rafael Angonese <rafael.angonese09@gmail.com>
2022-09-17 18:53:30 -0300

    :sparkles: feat: use InputText component with rhf

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,21 @@
+import InputText from "./components/Forms/InputText/InputeText";
 import ReackHookForm from "./components/ReackHookForm/ReactHookForm";
 
 function App() {
   return (
     <>
-      <div className="bg-pink-500 h-screen w-full">
+      <div className=" h-screen w-full px-8">
         Hello World
         <ReackHookForm />
+        <hr className="my-8" />
+        <InputText
+          isInvalid={false}
+          id="first_name"
+          label="Teste"
+          placeholder="Teste"
+          helperText="Teste"
+          required
+        />
       </div>
     </>
   );
--- react/src/components/ReackHookForm/ReactHookForm.tsx
+++ react/src/components/ReackHookForm/ReactHookForm.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import yup from "../../lib/yup";
+import InputText from "../Forms/InputText/InputeText";
 
 interface IFormInputs {
   name: string;
+  email: string;
   age: number;
 }
 
 const INITIAL_FORM_STATE = {
   name: "",
+  email: "",
   age: 0,
 };
 
 const FORM_VALIDATION = yup
   .object({
     name: yup.string().required(),
+    email: yup.string().email().required(),
     age: yup.number().positive().integer().required(),
   })
   .required();
@@ -26,6 +30,7 @@ const ReactHookForm: React.FC = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<IFormInputs>({
+    mode: "onChange",
     defaultValues: INITIAL_FORM_STATE,
     resolver: yupResolver(FORM_VALIDATION),
   });
@@ -43,6 +48,17 @@ const ReactHookForm: React.FC = () => {
 
         <input {...register("age")} />
         <p>{errors.age?.message}</p>
+
+        <div>
+          <InputText
+            {...register("email")}
+            label="E-mail"
+            isInvalid={!!errors.email}
+            placeholder="fuselagem@example.com"
+            helperText={errors.email?.message}
+            required
+          />
+        </div>
 
         <input type="submit" />
       </form>
