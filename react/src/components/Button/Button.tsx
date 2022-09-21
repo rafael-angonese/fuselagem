03661b885446d78868673c3a9585338e9cd54a23
Rafael Angonese <rafael.angonese09@gmail.com>
2022-09-20 23:44:33 -0300

    :sparkles: feat: add size into Button component

--- react/src/components/Button/Button.tsx
+++ react/src/components/Button/Button.tsx
@@ -3,15 +3,18 @@ import {
   GradientDuoToneColorsEnum,
   NormalColorsEnum,
 } from "../../constants/colors";
+import { SizesEnum } from "../../constants/sizes";
 import {
   GradientDuoToneColorsType,
   NormalColorsType,
 } from "../../types/colors";
+import { Sizes } from "../../types/sizes";
 import clsx from "../../utils/clsx";
 import LoadingIcon from "../Icons/LoadingIcon";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color?: NormalColorsType;
+  size?: Sizes;
   gradientDuoTone?: GradientDuoToneColorsType;
   isDisabled?: boolean;
   isLoading?: boolean;
@@ -19,6 +22,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const Button: React.FC<ButtonProps> = ({
   color = "default",
+  size = "md",
   className: classes,
   gradientDuoTone,
   isDisabled,
@@ -32,7 +36,7 @@ const Button: React.FC<ButtonProps> = ({
         {...props}
         disabled={isDisabled}
         className={clsx(
-          "text-white focus:ring-4 font-medium rounded-xl text-sm px-5 py-2.5",
+          "text-white rounded-lg text-center font-medium focus:ring-2",
           {
             ["bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"]:
               color === NormalColorsEnum.DEFAULT,
@@ -46,24 +50,31 @@ const Button: React.FC<ButtonProps> = ({
               color === NormalColorsEnum.ERROR,
             ["bg-yellow-700 hover:bg-yellow-800 focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800"]:
               color === NormalColorsEnum.WARNING,
-
-            ["text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"]:
-              gradientDuoTone === GradientDuoToneColorsEnum.PURPLE_TO_BLUE,
-            ["text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"]:
-              gradientDuoTone === GradientDuoToneColorsEnum.CYAN_TO_BLUE,
-            ["text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"]:
-              gradientDuoTone === GradientDuoToneColorsEnum.GREEN_TO_BLUE,
-            ["text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"]:
-              gradientDuoTone === GradientDuoToneColorsEnum.PURPLE_TO_PINK,
-            ["text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"]:
-              gradientDuoTone === GradientDuoToneColorsEnum.PINK_TO_ORANGE,
-            ["text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"]:
-              gradientDuoTone === GradientDuoToneColorsEnum.TEAL_TO_LIME,
-            ["text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"]:
-              gradientDuoTone === GradientDuoToneColorsEnum.RED_TO_YELLOW,
-
-            ["opacity-50 cursor-not-allowed"]: isDisabled,
           },
+          {
+            ["bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"]:
+              gradientDuoTone === GradientDuoToneColorsEnum.PURPLE_TO_BLUE,
+            ["bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800"]:
+              gradientDuoTone === GradientDuoToneColorsEnum.CYAN_TO_BLUE,
+            ["bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"]:
+              gradientDuoTone === GradientDuoToneColorsEnum.GREEN_TO_BLUE,
+            ["bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"]:
+              gradientDuoTone === GradientDuoToneColorsEnum.PURPLE_TO_PINK,
+            ["bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"]:
+              gradientDuoTone === GradientDuoToneColorsEnum.PINK_TO_ORANGE,
+            ["bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700"]:
+              gradientDuoTone === GradientDuoToneColorsEnum.TEAL_TO_LIME,
+            ["bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400"]:
+              gradientDuoTone === GradientDuoToneColorsEnum.RED_TO_YELLOW,
+          },
+          {
+            ["py-2 px-3 text-xs"]: size === SizesEnum.XS,
+            ["py-2 px-3 text-sm"]: size === SizesEnum.SM,
+            ["px-5 py-2.5 text-sm"]: size === SizesEnum.MD,
+            ["py-3 px-5 text-base"]: size === SizesEnum.LG,
+            ["py-6 px-6 text-base"]: size === SizesEnum.XL,
+          },
+          { "opacity-50 cursor-not-allowed": isDisabled },
           [classes]
         )}
       >
