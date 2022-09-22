795a50a95aa13016997299299619ddb038f8232a
Rafael Angonese <rafael.angonese09@gmail.com>
2022-09-22 11:37:39 -0300

    :sparkles: feat: add switch examples

--- react/src/pages/Home/Home.tsx
+++ react/src/pages/Home/Home.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import ButtonsExample from "./ButtonsExample/ButtonsExample";
 import ModalExample from "./ModalExample/ModalExample";
 import ReactHookForm from "./ReackHookForm/ReactHookForm";
+import SwitchExample from "./SwitchExample/SwitchExample";
 
 const HomePage: React.FC = () => {
   return (
     <>
       <ReactHookForm />
 
+      <SwitchExample />
+
       <ButtonsExample />
 
-      {/* <ModalExample /> */}
+      <ModalExample />
     </>
   );
 };
--- react/src/pages/Home/ReackHookForm/ReactHookForm.tsx
+++ react/src/pages/Home/ReackHookForm/ReactHookForm.tsx
@@ -1,29 +1,34 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import Button from "../../../components/Button/Button";
 import FormControl from "../../../components/Forms/FormControl/FormControl";
 import FormErrorMessage from "../../../components/Forms/FormErrorMessage/FormErrorMessage";
 import FormLabel from "../../../components/Forms/FormLabel/FormLabel";
 import InputText from "../../../components/Forms/InputText/InputeText";
+import Switch from "../../../components/Switch/Switch";
 import yup from "../../../lib/yup";
 
 interface IFormInputs {
   name: string;
   email: string;
   age: number;
+  airplane: boolean;
 }
 
 const INITIAL_FORM_STATE = {
   name: "",
   email: "",
-  age: 0,
+  age: 1,
+  airplane: false,
 };
 
 const FORM_VALIDATION = yup
   .object({
-    name: yup.string().required(),
+    // name: yup.string().required(),
     email: yup.string().email().required(),
-    age: yup.number().positive().integer().required(),
+    // age: yup.number().positive().integer().required(),
+    airplane: yup.boolean().required().oneOf([true]),
   })
   .required();
 
@@ -48,6 +53,7 @@ const Lala: React.FC = () => {
 const ReactHookForm: React.FC = () => {
   const {
     register,
+    control,
     handleSubmit,
     formState: { errors },
   } = useForm<IFormInputs>({
@@ -57,18 +63,18 @@ const ReactHookForm: React.FC = () => {
   });
   const onSubmit = (data: IFormInputs) => console.log(data);
 
+  console.log(errors);
+
   return (
     <>
       <h1>Reack Hook Form</h1>
 
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="my-4">
-          <FormLabel>E-mail</FormLabel>
+          <FormLabel>Nome</FormLabel>
           <InputText
             {...register("name")}
-            placeholder="fuselagem@example.com"
-            leftIcon={<Lala />}
-            rightIcon={<Lala />}
+            placeholder="fulaninho"
             isClearable
           />
         </div>
@@ -86,6 +92,28 @@ const ReactHookForm: React.FC = () => {
             <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
           </FormControl>
         </div>
+
+        <div className="my-4">
+          <FormControl
+            className="flex items-center"
+            isInvalid={!!errors.airplane}
+          >
+            <FormLabel>Modo viaom</FormLabel>
+            <Controller
+              control={control}
+              name="airplane"
+              render={({ field }) => (
+                <Switch
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              )}
+            />
+            <FormErrorMessage>{errors.airplane?.message}</FormErrorMessage>
+          </FormControl>
+        </div>
+
+        <Button>Submit</Button>
       </form>
     </>
   );
