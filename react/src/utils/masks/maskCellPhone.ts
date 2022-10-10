552ae89b19aa5cfc67e0ffb6883dd7ee0fd7f139
Rafael Angonese <rafael.angonese09@gmail.com>
2022-10-10 16:52:49 -0300

    :recycle: refactor(mask phone): created maskCellPhone mask and changed maskPhone mask

--- react/src/utils/masks/maskCellPhone.ts
+++ react/src/utils/masks/maskCellPhone.ts
@@ -1,24 +1,27 @@
 /* Examples
-  maskPhone(99933333333)   => (99) 9 3333-3333
+  maskCellPhone(99933333333)   => (99) 9 3333-3333
 */
 import isPresent from "../isPresent";
 
-const maskPhone = (value: string): string => {
+const maskCellPhone = (value: string): string => {
   let masked = value;
   if (isPresent(masked)) {
     masked = masked.toString();
     masked = masked.replace(/\D/g, "");
     masked = masked.replace(/^(\d)/, "($1");
     masked = masked.replace(/(.{3})(\d)/, "$1) $2");
-    if (masked.length === 10) {
+    if (masked.length > 6) {
+      masked = masked.replace(/(.{5})(\d)/, "$1$2 ");
+    }
+    if (masked.length === 12) {
       masked = masked.replace(/(.{1})$/, "-$1");
-    } else if (masked.length === 11) {
-      masked = masked.replace(/(.{2})$/, "-$1");
-    } else if (masked.length === 12) {
-      masked = masked.replace(/(.{3})$/, "-$1");
     } else if (masked.length === 13) {
-      masked = masked.replace(/(.{4})$/, "-$1");
+      masked = masked.replace(/(.{2})$/, "-$1");
     } else if (masked.length === 14) {
+      masked = masked.replace(/(.{3})$/, "-$1");
+    } else if (masked.length === 15) {
+      masked = masked.replace(/(.{4})$/, "-$1");
+    } else if (masked.length === 16) {
       masked = masked.replace(/(.{5})$/, "-$1");
     }
     masked = masked.replace(/(-\d{4})\d+?$/, "$1");
@@ -27,4 +30,4 @@ const maskPhone = (value: string): string => {
   return masked;
 };
 
-export default maskPhone;
+export default maskCellPhone;
