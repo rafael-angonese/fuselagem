dd5aaf7a0a6dd6a442625d7a0aad2f2e3ff8dba4
Rafael Angonese <rafael.angonese09@gmail.com>
2022-10-10 16:51:46 -0300

    :sparkles: feat(yup): add label into validation messages and created emptyAsUndefined method

--- react/src/lib/yup.ts
+++ react/src/lib/yup.ts
@@ -1,50 +1,79 @@
 // import { setLocale } from "yup";
 import * as yup from "yup";
+import { AnyObject, Maybe } from "yup/lib/types";
 
 const translation = {
   mixed: {
-    default: "é inválido",
-    required: "é um campo obrigatório",
-    oneOf: "deve ser um dos seguintes valores: ${values}",
-    notOneOf: "não pode ser um dos seguintes valores: ${values}",
-    defined: "não deve ser indefinido",
-    notType: "deve corresponder ao formato esperado",
-    // notType: "deve ser do tipo ${type}",
+    default: "${path} é inválido",
+    required: "${path} é um campo obrigatório",
+    oneOf: "${path} deve ser um dos seguintes valores: ${values}",
+    notOneOf: "${path} não pode ser um dos seguintes valores: ${values}",
+    defined: "${path} não deve ser indefinido",
+    notType: "${path} deve corresponder ao formato esperado",
+    // notType: "${path} deve ser do tipo ${type}",
   },
   string: {
-    length: "deve ter exatamente ${length} caracteres",
-    min: "deve ter pelo menos ${min} caracteres",
-    max: "deve ter no máximo ${max} caracteres",
-    matches: "campo deve corresponder ao formato esperado",
-    email: "deve ter um formato de e-mail válido",
-    url: "deve ter um formato de URL válida",
-    trim: "não deve conter espaços no início ou no fim.",
-    lowercase: "deve estar em letras maiúsculas",
-    uppercase: "deve estar em letras minúsculas",
+    length: "${path} deve ter exatamente ${length} caracteres",
+    min: "${path} deve ter pelo menos ${min} caracteres",
+    max: "${path} deve ter no máximo ${max} caracteres",
+    matches: "${path} campo deve corresponder ao formato esperado",
+    email: "${path} deve ter um formato de e-mail válido",
+    url: "${path} deve ter um formato de URL válida",
+    trim: "${path} não deve conter espaços no início ou no fim.",
+    lowercase: "${path} deve estar em letras maiúsculas",
+    uppercase: "${path} deve estar em letras minúsculas",
   },
   number: {
-    min: "deve ser maior ou igual a ${min}",
-    max: "deve ser menor ou igual a ${max}",
-    lessThan: "deve ser menor que ${less}",
-    moreThan: "deve ser maior que ${more}",
-    notEqual: "não pode ser igual à ${notEqual}",
-    positive: "deve ser um número positivo",
-    negative: "deve ser um número negativo",
-    integer: "deve ser um número inteiro",
+    min: "${path} deve ser maior ou igual a ${min}",
+    max: "${path} deve ser menor ou igual a ${max}",
+    lessThan: "${path} deve ser menor que ${less}",
+    moreThan: "${path} deve ser maior que ${more}",
+    notEqual: "${path} não pode ser igual à ${notEqual}",
+    positive: "${path} deve ser um número positivo",
+    negative: "${path} deve ser um número negativo",
+    integer: "${path} deve ser um número inteiro",
   },
   date: {
-    min: "deve ser posterior a ${min}",
-    max: "deve ser anterior a data ${max}",
+    min: "${path} deve ser posterior a ${min}",
+    max: "${path} deve ser anterior a data ${max}",
   },
   object: {
-    noUnknown: "tem chaves desconhecidas: ${unknown}",
+    noUnknown: "${path} tem chaves desconhecidas: ${unknown}",
   },
   array: {
-    min: "deve ter no mínimo ${min} itens",
-    max: "deve ter no máximo ${max} itens",
+    min: "${path} deve ter no mínimo ${min} itens",
+    max: "${path} deve ter no máximo ${max} itens",
   },
 };
 
 yup.setLocale(translation);
+
+yup.addMethod<yup.StringSchema>(yup.string, "emptyAsUndefined", function () {
+  return this.transform((value) => (value ? value : undefined));
+});
+
+yup.addMethod<yup.NumberSchema>(yup.number, "emptyAsUndefined", function () {
+  return this.transform((value, originalValue) =>
+    String(originalValue)?.trim() ? value : undefined
+  );
+});
+
+declare module "yup" {
+  interface StringSchema<
+    TType extends Maybe<string> = string | undefined,
+    TContext extends AnyObject = AnyObject,
+    TOut extends TType = TType
+  > extends yup.BaseSchema<TType, TContext, TOut> {
+    emptyAsUndefined(): StringSchema<TType, TContext>;
+  }
+
+  interface NumberSchema<
+    TType extends Maybe<number> = number | undefined,
+    TContext extends AnyObject = AnyObject,
+    TOut extends TType = TType
+  > extends yup.BaseSchema<TType, TContext, TOut> {
+    emptyAsUndefined(): NumberSchema<TType, TContext>;
+  }
+}
 
 export default yup;
