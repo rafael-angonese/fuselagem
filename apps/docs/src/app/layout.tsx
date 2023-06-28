a1f632fc21c295bb444f15925db5eaae58fd7f49
Rafael Angonese <rafael.angonese09@gmail.com>
2023-06-28 19:14:17 -0300

    :sparkles: feat(docs): add toggle theme options

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,6 +1,6 @@
+import Header from "@/components/Header/Header";
 import "fuselagem/styles.css";
 import { JetBrains_Mono } from "next/font/google";
-import Link from "next/link";
 import "./globals.css";
 
 const font = JetBrains_Mono({
@@ -19,25 +19,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" className={font.className}>
-      <body className="bg-black text-zinc-300">
-        <header className="border-b p-2 border-zinc-300 sticky top-0 left-0 right-0 z-10 bg-black">
-          <nav className="container mx-auto flex justify-between items-center">
-            <Link
-              href={"/"}
-              className="font-extrabold text-transparent sm:text-2xl text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
-            >
-              FuselagemUI
-            </Link>
-            <div className="text-base flex gap-8">
-              <Link href={"/docs"}>Docs</Link>
-              <Link href={"/docs/components"}>Components</Link>
-            </div>
-            <div className="text-base flex gap-2">
-              <span>Github</span>
-              <span>Theme</span>
-            </div>
-          </nav>
-        </header>
+      <body className="bg-white dark:bg-black text-black dark:text-zinc-300">
+        <Header />
         <div className="container mx-auto mt-14">{children}</div>
       </body>
     </html>
---
+++ apps/docs/src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import ToggleTheme from "@/components/Header/ToggleTheme";
+import Link from "next/link";
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <header className="border-b p-2 border-zinc-300 sticky top-0 left-0 right-0 z-10 bg-slate-50 dark:bg-black">
+        <nav className="container mx-auto flex justify-between items-center">
+          <Link
+            href={"/"}
+            className="font-extrabold text-transparent sm:text-2xl text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+          >
+            FuselagemUI
+          </Link>
+          <div className="text-base flex gap-8">
+            <Link href={"/docs"}>Docs</Link>
+            <Link href={"/docs/components"}>Components</Link>
+          </div>
+          <div className="text-base flex gap-2">
+            <span>Github</span>
+            <ToggleTheme />
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
