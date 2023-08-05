01f791142ddc8a2cbafc7dd2a55a9880befe8399
Rafael Angonese <rafael.angonese09@gmail.com>
2023-08-05 15:44:04 -0300

    :sparkles: feat: add pros cards

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,6 +1,14 @@
-import Card from "@/components/Card/Card";
+import { Card } from "@/components/Card/Card";
 import { Button, InputText } from "fuselagem";
-import { Search } from "lucide-react";
+import {
+  Atom,
+  Code,
+  Copy,
+  FileType,
+  MoonStar,
+  PersonStanding,
+  Search,
+} from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -25,10 +33,82 @@ export default function Home() {
             placeholder="Quick search"
           />
         </div>
+      </div>
 
-        <div className="mt-10 flex gap-8">
-          <Card />
-          <Card />
+      <div className="mt-56">
+        <h1 className="text-center text-4xl sm:text-5xl lg:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+          A experiência que você deseja
+        </h1>
+        <div className=" my-10 flex justify-center flex-wrap gap-8">
+          <Card.Root>
+            <Card.Icon>
+              <Copy size={30} />
+            </Card.Icon>
+            <Card.Title>Design by copy/paste</Card.Title>
+            <Card.Text>
+              FuselagemUI NÃO é uma biblioteca de componentes. Escolha o
+              componentes que você precisa. Copie e cole o código em seu projeto
+              e personalize de acordo com suas necessidades. O código é seu...
+            </Card.Text>
+          </Card.Root>
+
+          <Card.Root>
+            <Card.Icon>
+              <MoonStar size={30} />
+            </Card.Icon>
+            <Card.Title>Light & Dark UI</Card.Title>
+            <Card.Text>
+              Reconhecimento automático do modo escuro, FuselagemUI altera
+              automaticamente o tema quando detecta alterações de propriedade do
+              tema HTML. Use modo claro ou escuro, a sua escolha.
+            </Card.Text>
+          </Card.Root>
+
+          <Card.Root>
+            <Card.Icon>
+              <PersonStanding size={30} />
+            </Card.Icon>
+            <Card.Title>Accessible</Card.Title>
+            <Card.Text>
+              FuselagemUI segue estritamente os padrões WAI-ARIA para todos os
+              componentes. Os componentes são construídos sobre o React Aria
+              garantindo suporte de acessibilidade excepcional como prioridade
+              máxima.
+            </Card.Text>
+          </Card.Root>
+
+          <Card.Root>
+            <Card.Icon>
+              <FileType size={30} />
+            </Card.Icon>
+            <Card.Title>TypeScript based</Card.Title>
+            <Card.Text>
+              Crie aplicações seguras de tipagem, FuselagemUI tem uma API
+              totalmente tipada para minimizar a curva de aprendizado e ajudá-lo
+              a criar aplicações.
+            </Card.Text>
+          </Card.Root>
+
+          <Card.Root>
+            <Card.Icon>
+              <Code size={30} />
+            </Card.Icon>
+            <Card.Title>Fast</Card.Title>
+            <Card.Text>
+              Construído com TailwindCSS, o que significa que não há runtime
+              styles, nem classes desnecessárias em seu bundle.
+            </Card.Text>
+          </Card.Root>
+
+          <Card.Root>
+            <Card.Icon>
+              <Atom size={30} />
+            </Card.Icon>
+            <Card.Title>React server components</Card.Title>
+            <Card.Text>
+              Todos os componentes já incluem a diretiva use client
+            </Card.Text>
+          </Card.Root>
         </div>
       </div>
     </main>
--- apps/docs/src/components/Card/Card.tsx
+++ apps/docs/src/components/Card/Card.tsx
@@ -1,30 +1,64 @@
-import React from "react";
+import { ReactNode } from "react";
 
 import "./styles.css";
-import { MoonStar } from "lucide-react";
 
-const Card: React.FC = () => {
+interface RootProps {
+  children?: ReactNode;
+}
+
+function Root({ children }: RootProps) {
   return (
     <>
-      <div className="card relative inline-block rounded-2xl p-1/2 overflow-hidden cursor-pointer">
+      <div className="card relative flex w-[400px] rounded-2xl p-1/2 overflow-hidden cursor-pointer">
         <div className="border animate_border"></div>
 
-        <div className="content shadow-[inset_0px_0px_0px_1px_rgb(50,50,50)] relative rounded-[inherit] p-[1rem 0.75rem] min-w-[484px] min-h-[348px] flex flex-col items-center justify-center text-center">
-          <div className="relative">
-            <div className="absolute -inset-1/3 aspect-[1/1] bg-red-500 blur-[140px]"></div>
-            <div className="relative w-[84px] aspect-[1/1] bg-black rounded-[100%] border-[1px] border-gray-500 flex justify-center items-center">
-              <MoonStar className="text-white" size={36} />
-            </div>
-          </div>
-          <h1 className="mt-8 font-bold">Light and Dark UI</h1>
-          <p className="max-w-[60%] mt-3 text-slate-400">
-            Automatic dark mode recognition, FuselagemUI automatically changes
-            the theme when detects HTML theme prop changes.
-          </p>
+        <div className="p-8 w-full shadow-[inset_0px_0px_0px_1px_rgb(50,50,50)] relative rounded-[inherit] p-[1rem 0.75rem] flex flex-col items-center text-center">
+          {children}
         </div>
       </div>
     </>
   );
-};
+}
 
-export default Card;
+interface IconProps {
+  children?: ReactNode;
+}
+
+function Icon({ children }: IconProps) {
+  return (
+    <>
+      <div className="relative">
+        <div className="absolute -inset-1/3 aspect-[1/1] bg-red-500 blur-[140px]"></div>
+        <div className="relative w-[84px] aspect-[1/1] bg-black rounded-[100%] border-[1px] border-gray-500 flex justify-center items-center">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
+
+interface TitleProps {
+  children?: ReactNode;
+}
+
+function Title({ children }: TitleProps) {
+  return (
+    <>
+      <h1 className="mt-8 font-bold">{children}</h1>
+    </>
+  );
+}
+
+interface TextProps {
+  children?: ReactNode;
+}
+
+function Text({ children }: TextProps) {
+  return (
+    <>
+      <p className="mt-3 text-slate-400">{children}</p>
+    </>
+  );
+}
+
+export const Card = { Root, Icon, Title, Text };
