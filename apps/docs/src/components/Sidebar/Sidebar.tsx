a65b520526449fe8226a0db8ee530cf3d81e9ea3
Rafael Angonese <rafael.angonese09@gmail.com>
2023-09-02 21:53:25 -0300

    :recycle: refactor(Sidebar): organize imports

--- apps/docs/src/components/Sidebar/Sidebar.tsx
+++ apps/docs/src/components/Sidebar/Sidebar.tsx
@@ -6,33 +6,35 @@ const Sidebar: React.FC = () => {
     <aside className="w-64">
       <div className="fixed h-[80vh] w-64 overflow-y-auto">
         <ul className="flex flex-col gap-4 list-disc list-inside">
-          <ActiveLink href={"/docs/components/button"}>Button</ActiveLink>
-          <ActiveLink href={"/docs/components/text"}>Text</ActiveLink>
+          <ActiveLink href={"/docs/components/accordion"}>Accordion</ActiveLink>
           <ActiveLink href={"/docs/components/avatar"}>Avatar</ActiveLink>
-          <ActiveLink href={"/docs/components/pagination"}>
-            Pagination
-          </ActiveLink>
-          <ActiveLink href={"/docs/components/table"}>Table</ActiveLink>
           <ActiveLink href={"/docs/components/badge"}>Badge</ActiveLink>
-          <ActiveLink href={"/docs/components/input"}>Input</ActiveLink>
-          <ActiveLink href={"/docs/components/textarea"}>Textarea</ActiveLink>
+          <ActiveLink href={"/docs/components/button"}>Button</ActiveLink>
           <ActiveLink href={"/docs/components/checkbox"}>Checkbox</ActiveLink>
-          <ActiveLink href={"/docs/components/radio"}>Radio</ActiveLink>
-          <ActiveLink href={"/docs/components/tooltip"}>Tooltip</ActiveLink>
+          <ActiveLink href={"/docs/components/divider"}>Divider</ActiveLink>
+          <ActiveLink href={"/docs/components/grid"}>Grid</ActiveLink>
+          <ActiveLink href={"/docs/components/input"}>Input</ActiveLink>
+          <ActiveLink href={"/docs/components/label"}>Label</ActiveLink>
           <ActiveLink href={"/docs/components/linear-progress"}>
             Linear Progress
           </ActiveLink>
-          <ActiveLink href={"/docs/components/select"}>Select</ActiveLink>
-          <ActiveLink href={"/docs/components/modal"}>Modal</ActiveLink>
-          <ActiveLink href={"/docs/components/switch"}>Switch</ActiveLink>
-          <ActiveLink href={"/docs/components/label"}>Label</ActiveLink>
-          <ActiveLink href={"/docs/components/input-date"}>
-            Input Date
+          <ActiveLink href={"/docs/components/pagination"}>
+            Pagination
           </ActiveLink>
-          <ActiveLink href={"/docs/components/divider"}>Divider</ActiveLink>
+          <ActiveLink href={"/docs/components/radio"}>Radio</ActiveLink>
           <ActiveLink href={"/docs/components/skeleton"}>Skeleton</ActiveLink>
-          <ActiveLink href={"/docs/components/loading"}>Loading</ActiveLink>
-          <ActiveLink href={"/docs/components/grid"}>Grid</ActiveLink>
+          <ActiveLink href={"/docs/components/switch"}>Switch</ActiveLink>
+          <ActiveLink href={"/docs/components/table"}>Table</ActiveLink>
+          <ActiveLink href={"/docs/components/text"}>Text</ActiveLink>
+          <ActiveLink href={"/docs/components/textarea"}>Textarea</ActiveLink>
+          <ActiveLink href={"/docs/components/tooltip"}>Tooltip</ActiveLink>
+
+          {/* <ActiveLink href={"/docs/components/select"}>Select</ActiveLink> */}
+          {/* <ActiveLink href={"/docs/components/modal"}>Modal</ActiveLink> */}
+          {/* <ActiveLink href={"/docs/components/input-date"}>
+            Input Date
+          </ActiveLink> */}
+          {/* <ActiveLink href={"/docs/components/loading"}>Loading</ActiveLink> */}
         </ul>
       </div>
     </aside>
