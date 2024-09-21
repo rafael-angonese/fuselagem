5fe9cc57f6705250512ebbf1b0edf2381ca7fb56
Rafael Angonese <rafael.angonese09@gmail.com>
2024-09-21 17:21:46 -0300

    :sparkles: Feat: update site config

--- config/site.ts
+++ config/site.ts
@@ -1,41 +1,38 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "NextUI - Beautiful, fast and modern React UI Library",
+  name: "FuselagemUI - Beautiful, fast and modern React UI Library",
   description: "Make beautiful websites regardless of your design experience.",
   ogImage: "https://nextui.org/twitter-cards/nextui.jpeg",
-  author: "Junior Garcia",
-  email: "jrgarciadev@gmail.com",
-  siteUrl: "https://nextui.org",
-  creator: "@getnextui",
+  author: "Planegazers",
+  email: "planegazers@gmail.com",
+  siteUrl: "https://www.fuselagem.com.br",
+  creator: "@fuselagem-ui",
   openGraph: {
     type: "website",
     locale: "en_US",
     url: "https://nextui.org",
-    siteName: "NextUI",
+    siteName: "FuselagemUI",
     description: "Beautiful, fast and modern React UI Library",
     images: [
       {
         url: "https://nextui.org/twitter-cards/nextui.jpeg",
         width: 1200,
         height: 630,
-        alt: "NextUI",
+        alt: "FuselagemUI",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "NextUI - Beautiful, fast and modern React UI Library",
+    title: "FuselagemUI - Beautiful, fast and modern React UI Library",
     description: "Make beautiful websites regardless of your design experience.",
     image: "https://nextui.org/twitter-cards/nextui.jpeg",
-    creator: "@getnextui",
+    creator: "@fuselagem-ui",
   },
   links: {
-    github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui-docs-v2.vercel.app",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
-    portfolio: "https://jrgarciadev.com",
+    github: "https://github.com/rafael-angonese/fuselagem",
+    docs: "http://fuselagem.com.br",
+    portfolio: "https://www.planegazers.com",
   },
 };
