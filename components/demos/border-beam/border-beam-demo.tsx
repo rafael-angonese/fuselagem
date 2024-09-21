767b3f2a4d1c7d9ef78bb43bf8140f90bc2e0592
Rafael Angonese <rafael.angonese09@gmail.com>
2024-09-21 11:03:46 -0300

    :recycle: Refactor: lint classnames

--- components/demos/border-beam/border-beam-demo.tsx
+++ components/demos/border-beam/border-beam-demo.tsx
@@ -1,9 +1,19 @@
 import { BorderBeam } from "@/components/ui/border-beam/border-beam";
+import { cnBase } from "tailwind-variants";
 
 export default function BorderBeamDemo() {
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background md:shadow-xl border border-default-200 dark:border-default-100">
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+    <div className={cnBase(
+      "relative flex h-[500px] w-full flex-col items-center",
+      "justify-center overflow-hidden rounded-lg bg-background",
+      "md:shadow-xl border border-default-200 dark:border-default-100",
+    )}>
+      <span className={cnBase(
+        "pointer-events-none leading-none text-transparent",
+        "bg-gradient-to-b from-black to-gray-300/80",
+        "whitespace-pre-wrap bg-clip-text text-center text-8xl font-semibold",
+        "dark:from-white dark:to-slate-900/10",
+      )}>
         Border Beam
       </span>
       <BorderBeam size={250} duration={12} delay={9} />
