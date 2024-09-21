8fdef79fc46ba5a9c1c7186c1f0b68ec37eb94a7
Rafael Angonese <rafael.angonese09@gmail.com>
2024-09-21 14:13:50 -0300

    :page_facing_up: Update license

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://nextui.org",
+  siteUrl: "https://fuselagem.com.br",
   generateRobotsTxt: true,
   exclude: ["/examples/*"],
 };
