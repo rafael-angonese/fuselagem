3cd2d194fd70e4af145cf8d5d08864e7bf4c8758
Rafael Angonese <rafael.angonese09@gmail.com>
2024-10-04 19:34:30 -0300

    :recycle: Refactor(AnimatedCursor): standardized declaration

--- components/ui/animated-cursor/animated-cursor.tsx
+++ components/ui/animated-cursor/animated-cursor.tsx
@@ -1,15 +1,17 @@
-import { AnimatePresence, motion, useMotionValue } from 'framer-motion'
+import { AnimatePresence, motion, MotionValue, useMotionValue } from 'framer-motion'
 import React, { useEffect, useState } from 'react'
 import { cnBase } from 'tailwind-variants'
 
-export const Pointer = ({
-  children,
-  className,
-  name
-}: {
+export interface PointerProps {
   children: React.ReactNode
   className?: string
   name: string
+}
+
+export const Pointer: React.FC<PointerProps> = ({
+  children,
+  className,
+  name
 }) => {
   const x = useMotionValue(0)
   const y = useMotionValue(0)
@@ -46,7 +48,13 @@ export const Pointer = ({
   )
 }
 
-export const FollowPointer = ({ x, y, name }: { x: any; y: any; name: string }) => {
+export interface FollowPointerProps {
+  name: string
+  x: MotionValue<number>;
+  y: MotionValue<number>;
+}
+
+export const FollowPointer: React.FC<FollowPointerProps> = ({ x, y, name }) => {
   return (
     <>
       <motion.div
@@ -85,26 +93,31 @@ export const FollowPointer = ({ x, y, name }: { x: any; y: any; name: string }) 
   )
 }
 
-export const AnimatedCursor: React.FC<{ className?: string; text: string }> = ({ className, text }) => (
+export interface AnimatedCursorProps {
+  className?: string;
+  text: string
+}
+
+export const AnimatedCursor: React.FC<AnimatedCursorProps> = ({ className, text }) => (
   <motion.div
-     initial={{ translateX: '0', translateY: '0' }}
-     animate={{ translateX: ['0', '20px', '0'], translateY: ['0', '40px', '0'] }}
-     transition={{ duration: 4, repeat: Infinity, bounce: true }}
-     className={'relative z-[2] flex items-center gap-4'}>
-     <div
-        className={cnBase(
-           'w-fit rounded-full border border-red-500 bg-red-500 px-2 py-1 text-white',
-           className
-        )}>
-        {text}
-     </div>
-     <svg fill="none" height="18" viewBox="0 0 17 18" width="17">
-        <path
-           d="M15.5036 3.11002L12.5357 15.4055C12.2666 16.5204 10.7637 16.7146 10.22 15.7049L7.4763 10.6094L2.00376 8.65488C0.915938 8.26638 0.891983 6.73663 1.96711 6.31426L13.8314 1.65328C14.7729 1.28341 15.741 2.12672 15.5036 3.11002ZM7.56678 10.6417L7.56645 10.6416C7.56656 10.6416 7.56667 10.6416 7.56678 10.6417L7.65087 10.4062L7.56678 10.6417Z"
-           fill="#f31260"
-           stroke="#f31260"
-           strokeWidth="1.5"
-        />
-     </svg>
+    initial={{ translateX: '0', translateY: '0' }}
+    animate={{ translateX: ['0', '20px', '0'], translateY: ['0', '40px', '0'] }}
+    transition={{ duration: 4, repeat: Infinity, bounce: true }}
+    className={'relative z-[2] flex items-center gap-4'}>
+    <div
+      className={cnBase(
+        'w-fit rounded-full border border-red-500 bg-red-500 px-2 py-1 text-white',
+        className
+      )}>
+      {text}
+    </div>
+    <svg fill="none" height="18" viewBox="0 0 17 18" width="17">
+      <path
+        d="M15.5036 3.11002L12.5357 15.4055C12.2666 16.5204 10.7637 16.7146 10.22 15.7049L7.4763 10.6094L2.00376 8.65488C0.915938 8.26638 0.891983 6.73663 1.96711 6.31426L13.8314 1.65328C14.7729 1.28341 15.741 2.12672 15.5036 3.11002ZM7.56678 10.6417L7.56645 10.6416C7.56656 10.6416 7.56667 10.6416 7.56678 10.6417L7.65087 10.4062L7.56678 10.6417Z"
+        fill="#f31260"
+        stroke="#f31260"
+        strokeWidth="1.5"
+      />
+    </svg>
   </motion.div>
 )
